31bd45beca6a1a174f854a7ab4efbd9bc632837e
Praveen Dias <praveendias1180@gmail.com>
2022-04-09 09:54:55 +0530

    Can receive messages now 🚀

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const io = require('socket.io')(http, {
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-    socket.on('message', (socket) => {
+    socket.on('message', (message) => {
         console.log(message);
         io.emit('message', `${socket.id.substr(0, 2)} said ${message}`)
     })
