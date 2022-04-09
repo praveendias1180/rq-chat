59e1b7ba3d7b8ba1376ff3a892818daf4b00836f
Praveen Dias <praveendias1180@gmail.com>
2022-04-09 09:54:36 +0530

    Backend completed 🚀

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,15 @@
 const socket = io('ws://localhost:4000');
+
+
+socket.on('message', text => {
+    console.log(text);
+    const el = document.createElement('li');
+    el.innerHTML = text;
+    document.querySelector('ul').appendChild(el);
+})
+
+document.querySelector('button').onclick = () => {
+    const text = document.querySelector('input').value;
+    socket.emit('message', text);
+    document.title = text + " - RQ Chat App";
+}
